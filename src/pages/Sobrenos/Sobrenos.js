e31aac368ca1da74803fc9f35c65bb4812b9647a
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-03-05 13:55:20 -0300

    hoje

--- src/pages/Sobrenos/Sobrenos.js
+++ src/pages/Sobrenos/Sobrenos.js
@@ -1,16 +1,6 @@
 import c1 from "../../components/Img/c1.jpg";
 import c2 from "../../components/Img/c2.jpg";
-import {
-  SiLinkedin,
-  SiReact,
-  SiPrisma,
-  SiBootstrap,
-  SiSwagger,
-} from "react-icons/si";
-import { AiFillGithub } from "react-icons/ai";
-import { IoLogoJavascript } from "react-icons/io";
-import { FaCss3Alt } from "react-icons/fa";
-import { Cards, Social } from "./styles";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Sobrenos() {
@@ -21,79 +11,26 @@ export default function Sobrenos() {
           <div className="row">
             <div className="col">
               <br></br>
-              <h3 className="mx-3 my-3 text-center">BIOGRAFIA DOS DEVS</h3>
+              <h3 className="mx-3 my-3 text-center">BIOGRAFIA DA REDE DE CASAIS</h3>
+              <h3 className="mx-3 my-3 text-center">AMAI SEMPRE</h3>
             </div>
-          </div>
-          <p>
-            <h5>
-              <justify>
-                
-                O projeto Gerenciador Restoque visa automatizar o gerenciamento
-                de produtos da maneira mais otimizada possível, nosso
-                disponibiliza a melhor solução para gerenciamento de produtos!
-              </justify>
-            </h5>
-          </p>
-
-          <br></br>
+     
+            </div>
+         
+          
           <p>
             <h3>
-              <center> INFORMAÇÕES SOBRE O PROJETO:</center>
+              <center> INFORMAÇÕES SOBRE A LIDERANÇA:</center>
             </h3>
           </p>
 
-          <p>
-            <h5>
-              <center>
-                Aqui foram usadas as ferramentas para o desenvolvimento do nosso
-                projeto:
-              </center>
-            </h5>
-          </p>
-          <Social>
-            <a href="https://swagger.io/">
-              {" "}
-              <SiSwagger />
-            </a>
-            <h7>Swagger</h7>
-            <a href="https://www.prisma.io/">
-              {" "}
-              <SiPrisma />
-            </a>
-            <h7>Prisma</h7>
-            <a href="https://pt-br.reactjs.org/">
-              {" "}
-              <SiReact />
-            </a>
-            <h7>React</h7>
-            <a href="https://getbootstrap.com/">
-              {" "}
-              <SiBootstrap />
-            </a>
-            <h7>Bootstrap</h7>
-            <a href="https://github.com/uesleiMF">
-              {" "}
-              <FaCss3Alt />
-            </a>
-            <h7>Css</h7>
-            <a href="https://www.javascript.com/">
-              {" "}
-              <IoLogoJavascript />
-            </a>
-            <h7>Javascript</h7>
-          </Social>
-          <br></br>
-
-          <p>
-            <h2>
-              <center>Conheça nossa equipe:</center>
-            </h2>
-          </p>
+          </div>
+     
+     </div>
+  
 
           <div className="card mt-2 bg-success">
-  <h4 className="mx-3 my-3 text-center">ESCOLHA A CELULA MAIS PROXIMA DE SUA CASA </h4>
-  <h5 className="mx-3 my-3 text-center">CLIQUE NAS IMAGENS ABAIXO! </h5>
-        
+  
                
   
   <div className="row mb-3">
@@ -231,10 +168,6 @@ export default function Sobrenos() {
       
       </div>
         </div>
-        <div>
-          <br></br>
-        </div>
-      </div>
-    </div>
+       
   );
 }
