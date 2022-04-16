2efc5466b0e7f47541b0409f93280328a35ae9fd
Jose Ueslei De Moraes Ferreira <joseueslei27@gmail.com>
2022-04-15 21:52:45 -0300

    Update and rename src/pages/Cadastro/Cadastro.js to src/Páginas/Cadastro/Cadastro.js
    Alterações

--- src/Páginas/Cadastro/Cadastro.js
+++ src/Páginas/Cadastro/Cadastro.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Api from '../../api/api';
 import { useNavigate } from 'react-router-dom';
-
+import  "bootstrap/dist/css/bootstrap.min.css" ;
+importar  ListProdut  de  "../../components/structure/ListProdut" ;
 const Cadastro = () => {
   const navigate = useNavigate();
   
